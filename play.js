02b9fd64ebb04e82b55f04b1aa86fe3c87cc801a
AmyTang <huilitang1@gmail.com>
2021-07-28 21:16:02 -0700

    update

--- play.js
+++ play.js
@@ -1,33 +1,6 @@
-const net = require("net");
-const {connect} = require('./client');
+
 const setupInput = require('./input');
+const {connect} = require('./client');
 console.log("Connecting ...");
 
-const conn = connect();
-for (let i = 0; i < 20; i++) {
-    setTimeout(()=> {
-      conn.write('Name: Smile') 
-      conn.write('Move: up')
-    }, 300 * i)
-} 
-for (let i = 0; i < 20; i++) {
-  setTimeout(()=> {
-    conn.write('Name: Smile') 
-    conn.write('Move: right')
-  }, 500 * i)
-} 
-for (let i = 0; i < 20; i++) {
-    setTimeout(()=> {
-      conn.write('Name: Smile') 
-      conn.write('Move: left')
-    }, 700 * i)
-} 
-
-for (let i = 0; i < 20; i++) {
-    setTimeout(()=> {
-      conn.write('Name: Smile') 
-      conn.write('Move: down')
-    }, 900 * i)
-} 
-
-setupInput();
+setupInput(connect());
